4c7bf6b735c4eda9ff6d3f5baeba6ad715382cda
Jirat Sukphalanon <6210612633@cn330>
2021-09-16 21:06:42 +0700

    test

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -2,12 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Airport(models.Model):
-    code = models.CharField(max_length = 3)
-    city = models.CharField(max_length = 64)
 
-    def __str__(self):
-        return f"{self.city} ({self.code})"
 
 
 class Flight(models.Model):
