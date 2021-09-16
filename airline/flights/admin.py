f873aa3a9d7dd8a21b5a96ace43b56d043cd7f44
Jirapat maleehuan <6210612765@student.tu.ac.th>
2021-09-16 16:23:28 +0700

    up

---
+++ airline/flights/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Airport,Flight,Passenger
+
+class AirportAdmin(admin.ModelAdmin):
+    list_display = ("code", "city")
+
+
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id","origin","destination","duration")
+
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)
+
+admin.site.register(Airport , AirportAdmin)
+admin.site.register(Flight)
+admin.site.register(Passenger , PassengerAdmin)
+
+
+
+
+
+
+
+
+
+
---
+++ airline/users/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    pass
+
+
+def login_view(request):
+    pass
+
+
+def logout_view(request):
+    pass
